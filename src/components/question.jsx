a11dc653fea4c840754cbbbd14d003862e270c21
Maalik McBurrows <mmcburrows1@gmail.com>
2020-04-10 12:06:51 -0400

    created react app basic file structure

---
+++ src/components/question.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Question extends Component {
+    state = {
+        question: []
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Question;
